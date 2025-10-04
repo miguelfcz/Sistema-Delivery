bfa553383e7f85893e5ac47b9ebf973a8dc44dae
Miguel <miguelcf2006@gmail.com>
2025-10-04 10:32:18 -0300

    :arrow_up: implementando login com geração de token JWT

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import userRoute from './routes/userRoute';
+import authRoute from './routes/authRoute';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/users', userRoute);
+app.use('/usuarios', userRoute);
+app.use('/auth', authRoute);
 
 app.get('/', (req, res) => {
     res.send("Sistema de Delivery API")
