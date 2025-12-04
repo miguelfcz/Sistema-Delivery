a3cf6abaa5da2a72ce47088914d00057dceafc35
Miguel <miguelcf2006@gmail.com>
2025-12-04 01:52:00 -0300

    teste

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -19,6 +19,12 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
 
+const allowedOrigins = [
+    'http://localhost:5173', // Para desenvolvimento local
+  'http://localhost:3000', // Caso você use outra porta local
+  process.env.FRONTEND_URL,
+]
+
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/usuarios', userRoute);
@@ -29,6 +35,21 @@ app.use('/api/pedidos', pedidoRoute);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use('/api/upload', uploadRoute);
+
+app.use(cors({
+    origin: function (origin, callback) {
+      // Permite requisições sem origem (ex: apps mobile, Postman, ou requests locais)
+      if (!origin) return callback(null, true); 
+      
+      // Verifica se a origem da requisição está na lista de permitidas
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // Importante para lidar com cookies/sessões/JWTs
+  }));
 
 app.get('/', (req, res) => {
     res.send("Cardapio Digital API - Documentação disponível em /api-docs")
